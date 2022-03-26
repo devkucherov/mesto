2fcc1cb72caa9fe2f4abec5478d014219f3e4590
Pasha Kucherov <dev.kucherov@gmail.com>
2022-03-26 22:26:11 +0300

    feat:добавить открытие и закрытие всплывающего окна

--- script/script.js
+++ script/script.js
@@ -1,8 +1,25 @@
+let userName = document.querySelector('.profile__user-name'); // Находим поле имени в блоке profile
+let userJob = document.querySelector('.profile__user-job'); // Находим поле работы в блоке profile
+let editButton = document.querySelector('.edit-button'); // Находим кнопку редактирования данных в блоке profile
+let popupElement = document.querySelector('.pop-up'); // Находим само окно 
+let closeButton = document.querySelector('.pop-up__close-button'); // Нходим кнопку закрытия окна
+
 // Находим форму в DOM
-let formElement = document.querySelector('.pop-up__form');// Воспользуйтесь методом querySelector()
+let formElement = document.querySelector('.pop-up__form'); // Воспользуйтесь методом querySelector()
 // Находим поля формы в DOM
 let nameInput = document.querySelector('.pop-up__user-name'); // Воспользуйтесь инструментом .querySelector()
 let jobInput = document.querySelector('.pop-up__user-job'); // Воспользуйтесь инструментом .querySelector()
+
+function openPopup() {
+	popupElement.classList.remove('pop-up__open');
+}
+
+function closePopup() {
+	popupElement.classList.add('pop-up__open');
+}
+
+editButton.addEventListener('click', openPopup)
+closeButton.addEventListener('click', closePopup)
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -12,12 +29,11 @@ function formSubmitHandler (evt) {
                                                 // О том, как это делать, расскажем позже.
 
     // Получите значение полей jobInput и nameInput из свойства value
-    userName.textContent = nameInput.value;
-    
 
     // Выберите элементы, куда должны быть вставлены значения полей
 
     // Вставьте новые значения с помощью textContent
+
 }
 
 // Прикрепляем обработчик к форме:
